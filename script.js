9c2b2fde67e9c0e8eeed2d734bcaf9e082fdf4b5
Osamudiame Ruth Igbinoba <105964264+bluesense-core@users.noreply.github.com>
2022-12-02 17:26:31 +0100

    added functionality to website

---
+++ script.js
@@ -0,0 +1,35 @@
+const arrayOfGradients = [
+    ['#FBD3E9', '#BB377D'],
+    ['#00d2ff', '#3a7bd5'],
+    ['#f2709c', '#ff9472'],
+    ['#a73737', '#7a2828'],
+    ['#4b6cb7', '#182848'],
+    ['#C04848', '#480048'],
+    ['#5f2c82', '#49a09d'],
+    ['#232526', '#414345'],
+    ['#5C258D', '#4389A2'],
+    ['#4776E6', '#8E54E9'],
+];
+const arrLength = arrayOfGradients.length;
+const main = document.querySelector('main');
+const prevBtn = document.getElementById('prevBtn');
+const nextBtn = document.getElementById('nextBtn');
+let counter = 0;
+
+nextBtn.addEventListener('click', () => {
+    if (counter === arrLength) {
+        counter = 0;
+    }
+
+    main.style.background = `linear-gradient(to left, ${arrayOfGradients[counter][0]}, ${arrayOfGradients[counter][1]})`;
+    counter++;
+});
+
+prevBtn.addEventListener('click', () => {
+    if (counter === 0) {
+        counter = arrLength - 1;
+    }
+
+    main.style.background = `linear-gradient(to left, ${arrayOfGradients[counter][0]}, ${arrayOfGradients[counter][1]})`;
+    counter--;
+});
